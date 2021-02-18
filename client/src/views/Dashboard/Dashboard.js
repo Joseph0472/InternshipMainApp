ffc883f18503d29a14d6164655dbca12636d5044
Ajon <rushjp@yeah.net>
2021-02-19 00:01:29 +1300

    Version 1.0

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -5,56 +5,37 @@ import ChartistGraph from "react-chartist";
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
-import Store from "@material-ui/icons/Store";
 import Warning from "@material-ui/icons/Warning";
 import Done from "@material-ui/icons/Done";
 import DateRange from "@material-ui/icons/DateRange";
-import LocalOffer from "@material-ui/icons/LocalOffer";
-import Update from "@material-ui/icons/Update";
-import ArrowUpward from "@material-ui/icons/ArrowUpward";
 import Sync from "@material-ui/icons/Sync";
 import AccessTime from "@material-ui/icons/AccessTime";
-import Accessibility from "@material-ui/icons/Accessibility";
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import BugReport from "@material-ui/icons/BugReport";
-import Code from "@material-ui/icons/Code";
-import Cloud from "@material-ui/icons/Cloud";
 import SchoolIcon from '@material-ui/icons/School';
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import Table from "components/Table/Table.js";
-import Tasks from "components/Tasks/Tasks.js";
-import CustomTabs from "components/CustomTabs/CustomTabs.js";
 import Danger from "components/Typography/Danger.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-import { bugs, website, server } from "variables/general.js";
 import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from 'react';
-import GoogleLogin from 'react-google-login';
-import GoogleLogout from 'react-google-login';
 import Chart from 'chart.js';
-import { Doughnut, Pie } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 import { loadCom } from '../../redux/reducers/companyReducer'
 import { loadStu } from '../../redux/reducers/studentReducer'
 import { getAllHisData} from '../../api/historicalData'
-
-
-
-
-
+import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+import Success from "components/Typography/Success";
 import {
   dailySalesChart,
   emailsSubscriptionChart,
   completedTasksChart,
 } from "variables/charts.js";
 
-import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
-import Success from "components/Typography/Success";
 
 const useStyles = makeStyles(styles);
 
